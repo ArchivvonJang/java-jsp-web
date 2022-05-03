f4ac8465b997958aee2f1106c1ec5dff5aadcf10
A <A@DESKTOP-NKSGQVR>
2022-05-03 11:51:39 +0900

    feat: create initial java project

---
+++ javadev/src/basic/Hello2.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!"); //한줄 주석
+		System.out.print("안녕하세요.");
+		System.out.print("오라클 자바");
+		/*
+		 * 2줄 이상 주석
+		 * */
+	}
+
+}
